ebd9c0e6fb735555c00129bd258fb705510d3ecf
Nimira43 <n.rabone72@aol.com>
2025-04-14 18:08:32 +0100

    Corrected typos. Tested Tailwind - all okay.

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 export default function HomePage() {
   return (
-    <div className='p-4'></div>
+    <div className='p-4 bg-dark'>
+      <h1 className='text-3xl text-main logo'>Elderwood</h1>
+    </div>
   )
 }
