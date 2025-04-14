34c365a5f52129457d06f2c4148e4cc7ebe941fe
Nimira43 <n.rabone72@aol.com>
2025-04-14 18:26:33 +0100

    Changed Shadcn theme colour - setup done.

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function HomePage() {
   return (
     <div className='p-4 bg-dark'>
       <h1 className='text-3xl text-main logo'>Elderwood</h1>
+      <Button>Login</Button>
     </div>
   )
 }
