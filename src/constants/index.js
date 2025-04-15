316ee1503ac4cd6639a5f3903bb7a8f0e99cef98
Nimira43 <n.rabone72@aol.com>
2025-04-15 09:42:56 +0100

    Expanded the words array.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,10 @@
 export const words = [
   { text: 'Innovative', imgPath: '/images/ideas.svg' },
-  { text: 'Cutting-edge', imgPath: '/images/concepts.svg' }
+  { text: 'Cutting-edge', imgPath: '/images/concepts.svg' },
+  { text: 'Elegant', imgPath: '/images/designs.svg' },
+  { text: 'Code', imgPath: '/images/code.svg' },
+  { text: 'Robust', imgPath: '/images/ideas.svg' },
+  { text: 'User-focused', imgPath: '/images/concepts.svg' },
+  { text: 'Seamless', imgPath: '/images/designs.svg' },
+  { text: 'Versatile', imgPath: '/images/code.svg' }
 ]
