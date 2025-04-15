04f0aeedf97a17b28b1c4f860bca8231e2691235
Nimira43 <n.rabone72@aol.com>
2025-04-15 08:58:27 +0100

    Start of hero layout

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,13 @@ const Hero = () => {
       <div className='absolute top-0 left-0 z-10'>
         <img src='/images/bg.png' alt='background' />
       </div>
-      
+      <div className='hero-layout'>
+        <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+          <div className='flex flex-col gap-7'>
+
+          </div>
+        </header>
+      </div>
     </section>
   )
 }
