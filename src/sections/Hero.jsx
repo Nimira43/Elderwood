b5a09ca0297ec327a0bd7e1ec40e82dd2298ef4e
Nimira43 <n.rabone72@aol.com>
2025-04-15 09:10:43 +0100

    Created Hero text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,7 +7,11 @@ const Hero = () => {
       <div className='hero-layout'>
         <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
           <div className='flex flex-col gap-7'>
-
+            <div className="hero-text">
+              <h1>Crafting Innovative</h1>
+              <h1>solutions for the</h1>
+              <h1>Evolving Digital World.</h1>
+            </div>
           </div>
         </header>
       </div>
