f8030c3805cf84350cdc88211951f07e713c7dd2
Nimira43 <n.rabone72@aol.com>
2025-04-15 09:29:39 +0100

    Created constants/index.js and placed words array there. Imported words back into Hero.jsx

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export const words = [
+  { text: 'Innovative', imgPath: '/images/ideas.svg' },
+  { text: 'Cutting-edge', imgPath: '/images/concepts.svg' }
+]
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,4 @@
-const words = [
-  { text: 'Innovative', imgPath: '/images/ideas.svg' },
-  { text: 'Cutting-edge', imgPath: '/images/concepts.svg' }
-]
+import { words } from '../constants'
 
 const Hero = () => {
   return (
@@ -12,21 +9,22 @@ const Hero = () => {
       <div className='hero-layout'>
         <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
           <div className='flex flex-col gap-7'>
-            <div className="hero-text">
+            <div className='hero-text'>
               <h1>
                 Crafting
-                <span className="slide">
-                  <span className="wrapper">
+                <span className='slide'>
+                  <span className='wrapper'>
                     {words.map((word) => (
                       <span
                         key={word.text}
-                        className="flex items-center md:gap-3 gap-1 pb-2"
+                        className='flex items-center md:gap-3 gap-1 pb-2'
                       >
                         <img
                           src={word.imgPath}
                           alt={word.text}
-                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                          className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'
                         />
+                        <span>{word.text}</span>
                       </span>
                     ))}
                   </span>  
