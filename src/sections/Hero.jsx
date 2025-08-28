c45caaef291f26f21229c183507c2bf99e3b285a
Nimira43 <n.rabone72@aol.com>
2025-08-29 00:25:51 +0100

    Importing useGSAP into Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import Button from '../components/Button'
 import HeroExperience from '../components/HeroModels/HeroExperience'
 import { words } from '../constants'
+import { useGSAP } from '@gsap/react'
 
 const Hero = () => {
   return (
