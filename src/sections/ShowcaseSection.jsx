be7f512143eb079fcb039432d225b6c957bb379e
Nimira43 <n.rabone72@aol.com>
2025-09-10 00:54:14 +0100

    Setting up ShowcaseSection layout

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const ShowcaseSection = () => {
   return (
-    <div>
-      
+    <div id='work' className='app-showcase'>
+      <div className='w-full'>
+        <div className='showcaseLayout'>
+          
+        </div>
+      </div>     
     </div>
   )
 }
