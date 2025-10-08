9c80cbe603490b8307f1350693534ad4f2853d56
Nimira43 <n.rabone72@aol.com>
2025-10-08 11:35:04 +0100

    Import useGSAP

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { useGSAP } from '@gsap/react'
 
 gsap.registerPlugin(ScrollTrigger)
 
