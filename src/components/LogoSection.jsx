a2716602eeb116ffad5c9754492d23745f4855b0
Nimira43 <n.rabone72@aol.com>
2025-11-10 14:46:04 +0000

    Mapping through logo icons - 3

--- src/components/LogoSection.jsx
+++ src/components/LogoSection.jsx
@@ -1,9 +1,12 @@
 import { logoIconsList } from '../constants'
 
-const LogoIcon = () => {
+const LogoIcon = ({ icon }) => {
   return (
     <div className='flex-none flex-center marquee-item'>
-
+      <img 
+        src={icon.imgPath} 
+        alt={icon.name} 
+      />
     </div>
   )
 }
