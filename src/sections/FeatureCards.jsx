3a55c6c6d96164be4aa39ddee7740f89a99923fa
Nimira43 <n.rabone72@aol.com>
2025-11-28 22:00:20 +0000

    FeatureCards - 1

--- src/sections/FeatureCards.jsx
+++ src/sections/FeatureCards.jsx
@@ -1,8 +1,14 @@
+import { projects } from '../constants'
+
 const FeatureCards = () => {
   return ( 
-    <div className='w-full padding-g-lg'>
-      <div className='mx-auto grid-3-col'>
-        {}
+    <div className='w-full padding-x-lg'>
+      <div className='mx-auto grid-3-cols'>
+        {projects.map(({ imgPath, title, desc }) => (
+          <div>
+            {title}
+          </div>
+        ))}
       </div>
     </div>
   )
