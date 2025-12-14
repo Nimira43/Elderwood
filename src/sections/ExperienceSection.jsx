d46de5a9a5ed7f4cb30a46bafc92503adcaa896f
Nimira43 <n.rabone72@aol.com>
2025-12-14 23:22:02 +0000

    Created TitleHeader.jsx in components.

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -5,6 +5,7 @@ const ExperienceSection = () => {
       class='w-full md:mt-40 mt-20 section-padding xl:px-0'
     >
       <div className='w-full h-full md:px-20 px-5'>
+        
       </div>  
     </section>
   )
