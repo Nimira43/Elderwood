e4905f5679290103409f12a21df1d5a1670a3f50
Nimira43 <n.rabone72@aol.com>
2025-12-31 14:59:25 +0000

    Applying stars

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -3,7 +3,18 @@ const GlowCard = ({card, children}) => {
     <div className='card card-border timeline-card rounded-xl p-10'>
       <div className='glow' />
       <div>
+        {Array.from(
+          { length: 5 },
+          (_, i) => (
+            <img
+              key={i}
+              src='/images/star.png'
+              alt='star'
+              className='size-5'
+            />
+          )
         
+        )}
       </div>
     </div>
   )
