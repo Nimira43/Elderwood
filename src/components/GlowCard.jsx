e6219bf4de85961df042bb92b610a95e8ac6c36a
Nimira43 <n.rabone72@aol.com>
2025-12-31 18:17:37 +0000

    Applying GlowCard to the FeaturedCards section.

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -1,24 +1,23 @@
-const GlowCard = ({card, children}) => {
+const GlowCard = ({ card, children, showStars = true }) => {
   return (
     <div className='card card-border timeline-card rounded-xl p-10'>
       <div className='glow' />
-      <div className='flex items-center gap-1 mb-5'>
-        {Array.from(
-          { length: 5 },
-          (_, i) => (
+
+      {showStars && (
+        <div className='flex items-center gap-1 mb-5'>
+          {Array.from({ length: 5 }, (_, i) => (
             <img
               key={i}
               src='/images/star.png'
               alt='star'
               className='size-5'
             />
-          )
-        )}
-      </div>
+          ))}
+        </div>
+      )}
+
       <div className='mb-5'>
-        <p
-          className='text-white-50 text-lg'
-        >
+        <p className='text-white-50 text-lg'>
           {card.review}
         </p>
         {children}
@@ -28,3 +27,4 @@ const GlowCard = ({card, children}) => {
 }
 
 export default GlowCard
+
--- src/sections/FeatureCards.jsx
+++ src/sections/FeatureCards.jsx
@@ -1,28 +1,33 @@
+import GlowCard from '../components/GlowCard'
 import { projects } from '../constants'
 
 const FeatureCards = () => {
-  return ( 
+  return (
     <div className='w-full padding-x-lg'>
       <div className='mx-auto grid-3-cols text-center'>
         {projects.map(({ imgPath, title, desc }) => (
-          <div
+          <GlowCard
             key={title}
-            className='card-border rounded-xl p-8 flex  items-center flex-col gap-4'
+            card={{ review: '' }}
+            showStars={false}
           >
-            <div className='size-14 flex items-center justify-center rounded-full'>
-              <img
-                src={imgPath}
-                alt={title} 
-                className='w-14 h-14 object-cover'
-              />
+            <div className='flex items-center flex-col gap-4'>
+              <div className='size-14 flex items-center justify-center rounded-full'>
+                <img
+                  src={imgPath}
+                  alt={title}
+                  className='w-14 h-14 object-cover'
+                />
+              </div>
+
+              <h3 className='text-white text-2xl font-medium'>{title}</h3>
+              <p className='text-white-50 text-base leading-relaxed'>{desc}</p>
             </div>
-            <h3 className='text-white text-2xl font-medium'>{title}</h3>
-            <p className='text-white-50 text-base leading-relaxed'>{desc}</p>            
-          </div>
+          </GlowCard>
         ))}
       </div>
     </div>
   )
 }
- 
+
 export default FeatureCards
