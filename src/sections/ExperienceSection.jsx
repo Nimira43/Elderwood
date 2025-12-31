3ac07618336d4f5843d875181f025f229831dd62
Nimira43 <n.rabone72@aol.com>
2025-12-31 13:28:50 +0000

    Updated the way the LogoIcon is being mapped and rendered jin LogoSection.jsx

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,11 +1,12 @@
 import TitleHeader from '../components/TitleHeader'
 import { expCards } from '../constants'
+import GlowCard from '../components/GlowCard'
 
 const ExperienceSection = () => {
   return (
     <section
       id='experience'
-      class='w-full md:mt-40 mt-20 section-padding xl:px-0'
+      className='w-full md:mt-40 mt-20 section-padding xl:px-0'
     >
       <div className='w-full h-full md:px-20 px-5'>
         <TitleHeader
--- src/sections/LogoSection.jsx
+++ src/sections/LogoSection.jsx
@@ -20,17 +20,11 @@ const LogoSection = () => {
 
       <div className='marquee h-52'>
         <div className='marquee-box md:gap-12 gap-5'>
-          {logoIconsList.map((icon) => (
+          {[...logoIconsList, ...logoIconsList].map((icon, index) => (
             <LogoIcon 
-              key={icon.name}
+              key={`${icon.name}-${index}`}
               icon={icon}
-            />            
-          ))}
-          {logoIconsList.map((icon) => (
-            <LogoIcon 
-              key={icon.name}
-              icon={icon}
-            />            
+            />
           ))}
         </div>
       </div>
