6576adb02e4bf2d87186c820ac365d2cdd88f80a
Nimira43 <n.rabone72@aol.com>
2025-12-31 15:52:30 +0000

    Images and index.js refactors - 2

--- src/constants/index.js
+++ src/constants/index.js
@@ -18,14 +18,14 @@ const navLinks = [
 ]
 
 const words = [
-  { text: 'Innovative', imgPath: '/images/ideas.svg' },
-  { text: 'Cutting-edge', imgPath: '/images/concepts.svg' },
-  { text: 'Elegant', imgPath: '/images/designs.svg' },
-  { text: 'Code', imgPath: '/images/code.svg' },
-  { text: 'Robust', imgPath: '/images/ideas.svg' },
-  { text: 'User-focused', imgPath: '/images/concepts.svg' },
-  { text: 'Seamless', imgPath: '/images/designs.svg' },
-  { text: 'Versatile', imgPath: '/images/code.svg' }
+  { text: 'Innovative', imgPath: '/images/words-images/ideas.svg' },
+  { text: 'Cutting-edge', imgPath: '/images/words-images/concepts.svg' },
+  { text: 'Elegant', imgPath: '/images/words-images/designs.svg' },
+  { text: 'Code', imgPath: '/images/words-images/code.svg' },
+  { text: 'Versatile', imgPath: '/images/words-images/ideas.svg' },
+  { text: 'Seamless', imgPath: '/images/words-images/concepts.svg' },
+  { text: 'User-focused', imgPath: '/images/words-images/designs.svg' },
+  { text: 'Robust', imgPath: '/images/words-images/code.svg' }
 ]
 
 const counterItems = [
