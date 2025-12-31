b65d24139a17a0fba5350a79f4c55c948e6c9495
Nimira43 <n.rabone72@aol.com>
2025-12-31 15:26:25 +0000

    Adjustment made to expCards data

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -2,7 +2,7 @@ const GlowCard = ({card, children}) => {
   return (
     <div className='card card-border timeline-card rounded-xl p-10'>
       <div className='glow' />
-      <div>
+      <div className='flex items-center gap-1 mb-5'>
         {Array.from(
           { length: 5 },
           (_, i) => (
@@ -13,8 +13,15 @@ const GlowCard = ({card, children}) => {
               className='size-5'
             />
           )
-        
         )}
+      </div>
+      <div className='mb-5'>
+        <p
+          className='text-white-50 text-lg'
+        >
+          {card.review}
+        </p>
+        {children}
       </div>
     </div>
   )
